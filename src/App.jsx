3c59e8f74c117eebe762af37a9abe62449abb652
sajeeb5523 <sajeebaljabed1@gmail.com>
2026-07-07 10:15:53 +0600

    feat: add responsive hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Hero></Hero>
     </>
   )
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,40 @@
+
+import heroImg from '../../../src/assets/Banner-min.jpg';
+
+const Hero = () => {
+    return (
+        <div>
+            <section className="relative h-[660px] bg-cover bg-center">
+
+                <div
+                    className="absolute inset-0 bg-cover bg-center"
+                    style={{
+                        backgroundImage: `url(${heroImg})`,
+                        transform: "scaleX(-1)",
+                    }}
+                />
+
+                <div className="absolute inset-0 bg-black/55"></div>
+
+                <div className="relative z-10 flex items-center h-full max-w-7xl mx-auto px-6 lg:px-0">
+                    <div className="max-w-2xl text-white">
+                        <h1 className="text-xl md:text-5xl font-bold leading-tight">
+                            Bid on Unique Items from Around the World
+                        </h1>
+
+                        <p className="mt-6 text-lg text-gray-300 leading-relaxed">
+                            Discover rare collectibles, luxury goods, and vintage treasures
+                            in our curated auctions.
+                        </p>
+
+                        <button className="mt-8 btn bg-white text-black border-none rounded-full px-8 hover:bg-gray-200">
+                            Explore Auctions
+                        </button>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Hero;
